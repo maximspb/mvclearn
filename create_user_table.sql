d6285d0dbf704c30375d8f2076f0394f07e34cd1
maximspb <maxim.kuzn@gmail.com>
2018-03-03 05:35:55 +0300

    Добавлена миграция создания таблицы user

---
+++ create_user_table.sql
@@ -0,0 +1,9 @@
+USE `learnbase`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
